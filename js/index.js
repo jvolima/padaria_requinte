c719d03ad52dbe94626220bc527b0b3693104f31
jvolima <jvolima2004@gmail.com>
2022-01-19 18:40:20 -0300

    padaria

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function saudacao() {
+  const data = new Date();
+
+  const hour = data.getHours();
+
+  const saudacao = document.getElementById("saudacao");
+
+  if(hour >= 6 && hour < 12) {
+    saudacao.innerHTML = "Bom dia! Estamos abertos 😃"
+  } 
+  else if(hour > 12 && hour < 18) {
+    saudacao.innerHTML = "Boa tarde! Estamos abertos 😃"
+  }
+  else if(hour < 20) {
+    saudacao.innerHTML = "Boa noite! Estamos abertos 😃"
+  }
+  else if(hour > 20) {
+    saudacao.innerHTML = "Boa noite! Estamos fechados 🙁"
+  }
+}
